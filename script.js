b5dd5040ecf0e24a2087045e90c0e9e446666c42
Marveleecious <richardtimilehin05@gmail.com>
2022-10-02 23:42:48 -0700

    my first statement

---
+++ script.js
@@ -0,0 +1,38 @@
+function sum(a, b, y){
+          console.log(a + b + y);
+     }
+
+// sum(1,2,3);
+
+function minus(a, b, c){
+    console.log(a - b - c);
+}
+// minus(9,4,3);
+
+function multiply(a, b, c){
+    console.log(a * b * c);
+}
+// multiply(5,2,2);
+
+function divide(a, b, c){
+    console.log(a / b / c);
+}
+// divide(4,2,2);
+
+function square(a, b, c){
+    console.log(a ** b ** c);
+}
+// square(5,4,4);
+
+function modulo(a, b, c){
+    console.log(a % b % c);
+}
+// modulo(4,3,2);
+
+
+sum(1,2,3);
+multiply(5,2,2);
+minus(9,4,3);
+divide(4,2,2);
+square(5,4,4);
+modulo(4,3,2);
